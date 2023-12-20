09e51ad3a7ddbfe6d9143c8f8eb9160cf3115194
AnkitRGoud <ankitrm.goud@gmail.com>
2023-12-20 23:05:17 +0530

    Db connection and context file added

---
+++ Data/MatrimonyDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MatrimonyWebApi.Data
+{
+    public class MatrimonyDbContext : DbContext
+    {
+        public MatrimonyDbContext(DbContextOptions dbContextOptions): base(dbContextOptions)
+        {
+                
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using MatrimonyWebApi.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//adding connection string
+builder.Services.AddDbContext<MatrimonyDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("MatrimonyConnectionString")));
 
 var app = builder.Build();
 
